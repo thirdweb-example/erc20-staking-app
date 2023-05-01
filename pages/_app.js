e416753ac5a71caa2e1970b19b9ca638cf8cf35d
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-04-30 20:34:13 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 import "../styles/globals.css";
 
-// This is the chainId your dApp will work on.
+// This is the chain your dApp will work on.
 const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }) {
